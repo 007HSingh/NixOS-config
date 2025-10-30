39140bd63fac1823692ff17bf413274d3f7609ed
007HSingh <singhharsh25032008@gmail.com>
2025-10-30 17:12:11 +0530

    Markdown-preview plugin build error fixed

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -332,11 +332,11 @@ require("lazy").setup({
 	-- Markdown preview
 	{
 		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
 		build = function()
 			vim.fn["mkdp#util#install"]()
 		end,
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
 	},
 
 	-- ========================================
