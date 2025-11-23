b8fccec01fc779c2db663002975f82756bcc1827
007HSingh <singhharsh25032008@gmail.com>
2025-11-23 12:15:16 +0530

    Error related to the Docker installation fixed

--- configuration.nix
+++ configuration.nix
@@ -163,7 +163,7 @@
     enable = true;
     daemon.settings = {
       experimental = true;
-      defualt-address-pools = [
+      default-address-pools = [
         {
           base = "172.30.0.0/16";
           size = 24;
