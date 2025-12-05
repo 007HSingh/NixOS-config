b1a4abd8c49add514f768ba8505b49382baa125d
007HSingh <singhharsh25032008@gmail.com>
2025-12-05 21:38:04 +0530

    Power management for GPU disabled

--- modules/system/hardware.nix
+++ modules/system/hardware.nix
@@ -7,7 +7,7 @@
 
     nvidia = {
       modesetting.enable = true;
-      powerManagement.enable = true;
+      powerManagement.enable = false;
       powerManagement.finegrained = false;
       open = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
